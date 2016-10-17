c35d5e96b59852da717e3c4e74d792935bb0ce9a
LOULAPPE <veronsyl@wanadoo.fr>
2016-10-17 17:56:22 +0200

    RAS

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,9 @@ angular.module('app', ['ngRoute', 'ui.materialize'])
     .factory('sessionFactory', sessionFactory)
     .service('userService', userService)
     .service('adminService', adminService)
-    .service('enigmeService', enigmeService)
+    /**.service('enigmeService', enigmeService)**/
     .controller('mainController', mainController)
-    .controller('enigmeController', enigmeController)
+    /**.controller('enigmeController', enigmeController)**/
     .controller('navbarController', navbarController)
     .controller('loginController', loginController)
     .controller('adminController', adminController)
