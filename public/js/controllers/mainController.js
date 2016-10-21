1bd3869e74d07259ea2c70225685b39db5fc08d5
Arnaud Monachon <arnaud.monachon@gmail.com>
2016-10-21 10:59:24 +0200

    correction bug burger

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,18 +1,9 @@
 class mainController {
 
     constructor() {
-        $(".button-collapse").sideNav();
         $('#textarea1').val('New Text');
         $('#textarea1').trigger('autoresize');
         $('input#input_text, textarea#textarea1').characterCounter();
-        $(window).scroll(function() {
-            if ($(window).scrollTop() > $('#accueil').height()) {
-                $('.navbar-fixed').addClass('navbar-color');
-            }
-            if ($(window).scrollTop() < $('#accueil').height()) {
-                $('.navbar-fixed').removeClass('navbar-color');
-            }
-        });
 
     }
 }
